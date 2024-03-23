ea49d96abf32e11ffcef74ba3dc5b1bb3b93d396
Anirudh Kaushik <143798902+Anirudh1705@users.noreply.github.com>
2024-03-23 16:22:14 +0530

    Add files via upload

---
+++ DAA/fabonacciseries.py
@@ -0,0 +1,17 @@
+def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+
+    fib = [0, 1]
+    for i in range(2, n):
+        fib.append(fib[i-1] + fib[i-2])
+
+    return fib
+
+# Test the function
+n = 10
+print(f"The first {n} terms of the Fibonacci series are: {fibonacci(n)}")
