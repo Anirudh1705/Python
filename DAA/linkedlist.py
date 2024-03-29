989c26da614683bc27bf38fc7003374ac8836ad4
Anirudh Kaushik <143798902+Anirudh1705@users.noreply.github.com>
2024-03-29 12:21:29 +0530

    Add files via upload

---
+++ DAA/linkedlist.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+
+    def display(self):
+        elements = []
+        current = self.head
+        while current:
+            elements.append(current.data)
+            current = current.next
+        return elements
+    
+ll = LinkedList()
+ll.insert(1)
+ll.insert(2)
+ll.insert(3)
+print(ll.display())  # [1, 2, 3]
